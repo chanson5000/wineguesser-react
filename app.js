c44f2dec07e5466240c1833dcd7ced9ec58b402e
Cory <chanson5000@gmail.com>
2018-09-21 23:24:52 -0500

    Added database connection.

--- app.js
+++ app.js
@@ -9,25 +9,28 @@ const whiteWines = require('./routes/api/wines/white');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// TODO: Add database connection code.
+// Define and connect to the MongoDB.
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err));
 
 // Passport will be used later
 // Passport middleware
 // app.use(passport.initialize());
 // Passport config
-require('./config/passport')(passport);
+// require('./config/passport')(passport);
 
 // Our routes
 app.use('/api/wines/red', redWines);
 app.use('/api/wines/white', whiteWines);
 
-// When building on Cloud9 instance this value will change.
-const ip = "172.31.17.32";
-// Cloud9 env port is 8080
-const port = process.env.PORT;
+const ip = "127.0.0.1";
+const port = 5000;
 
 app.listen(port, ip, () => {
     console.log(`Server running... ${ip}:${port}`);

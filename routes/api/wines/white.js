8ff9a8259ab230920517c8a068531a1a4550ae28
Cory <chanson5000@gmail.com>
2018-09-21 23:38:09 -0500

    Added test route

--- routes/api/wines/white.js
+++ routes/api/wines/white.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const passport = require('passport');
+// const passport = require('passport');
 
 // Red Wine model
-const WhiteWine = require('../../../models/RedWine');
+const WhiteWine = require('../../../models/WhiteWine').WhiteWine;
+
+// @route   GET api/wines/white/test
+// @desc    Test white wines rout
+// @access  Public
+router.get('/test', (req, res) => res.json({msg: "This route works."}));
 
 // @route   GET api/wines/red
 // @desc    Get all red wines
 // @access  Public
-
 router.get('/', (req, res) => {
     const errors = {};
 

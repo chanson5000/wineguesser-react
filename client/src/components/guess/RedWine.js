a6cfe28a3e372a2cb116fb50bc65e1b23776faa8
Cory Hanson <chanson5000@gmail.com>
2018-09-24 11:23:24 -0500

    Workstation transfer.

--- client/src/components/guess/RedWine.js
+++ client/src/components/guess/RedWine.js
@@ -3,9 +3,35 @@ import React, {Component} from 'react';
 class RedWine extends Component {
   render() {
     return (
-        <div>
-          <h2>Guess a red wine.</h2>
-        </div>
+      <div className="container">
+        <h2>Select red wine characteristics.</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="color">Color</label>
+              <div className="form-check form-control-lg">
+                <input className="form-check-input" type="radio" name="color" id="colorGarnet"
+                       value="colorGarnet"/>
+                <label className="form-check-label" htmlFor="colorGarnet">
+                  Garnet
+                </label>
+              </div>
+              <div className="form-check form-control-lg">
+                <input className="form-check-input" type="radio" name="color" id="colorRuby"
+                       value="colorRuby"/>
+                <label className="form-check-label" htmlFor="colorRuby">
+                  Ruby
+                </label>
+              </div>
+              <div className="form-check form-control-lg">
+                <input className="form-check-input" type="radio" name="color" id="colorPurple"
+                       value="colorPurple"/>
+                <label className="form-check-label" htmlFor="colorPurple">
+                  Purple
+                </label>
+              </div>
+            </div>
+          </form>
+      </div>
     );
   }
 }
